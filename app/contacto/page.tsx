6132456da14e2e36957ec2a08235a35147adbac0
Eva Gandia Belda <147039955+evagandiabelda@users.noreply.github.com>
2025-07-20 12:42:39 +0200

    Es crea una primera versió dels botons a la pàgina de 'Contacto'.

--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Image from "next/image";
+
 export default function Contacto() {
 
     return (
@@ -8,7 +10,8 @@ export default function Contacto() {
             {/* Sidebar */}
 
             <div className="w-1/4 flex flex-col justify-start align-start gap-[2rem] px-[3rem] py-[5rem] bg-[var(--gris-claro)] rounded-tr-[4rem]">
-                <h1 className="text-[var(--gris-claro)]">Contacto</h1> {/* Oculto */}
+                <h1 className="hidden">Contacto</h1> {/* Oculto */}
+
                 <h3>¿Te interesa mi perfil profesional?</h3>
                 <p>Estaré encantada de que concertemos una entrevista.</p>
             </div>
@@ -17,20 +20,34 @@ export default function Contacto() {
 
             <div className="w-3/4 py-[5rem] flex flex-col justify-start items-center gap-[2rem]">
 
-                <div className="w-1/2">
+                <div className="w-2/3">
 
                     <div>
-                        <h2 className="text-[var(--gris-claro)]">Hablemos</h2>
+                        <h2 className="text-[var(--gris-medio)]">Hablemos</h2>
                     </div>
 
-                    <div className="flex flex-row justify-stretch items-stretch gap-[0.5rem]">
+                    <div className="flex flex-row justify-stretch items-stretch gap-[1.5rem]">
 
-                        <div className="w-full bg-[var(--gris-claro)] rounded-[1rem]">
-                            <p>...</p>
+                        {/* CARD TEL */}
+                        <div className="flex flex-row justify-start items-center gap-[1.6rem] bg-[#E8C6B2] rounded-full p-[0.6rem] hover:scale-[1.02] transition ease duration-300 cursor-pointer">
+                            <Image
+                                src="/icons/icono-tel.svg"
+                                alt="Icono Teléfono"
+                                width={36}
+                                height={36}
+                            />
+                            <p className="text-[1.1rem] font-[600] m-[0px] pr-[1.6rem]">(+34) 660 201 670</p>
                         </div>
 
-                        <div className="w-full bg-[var(--gris-claro)] rounded-[1rem]">
-                            <p>...</p>
+                        {/* CARD EMAIL */}
+                        <div className="flex flex-row justify-start items-center gap-[1.6rem] bg-[#E8C6B2] rounded-full p-[0.6rem] hover:scale-[1.02] transition ease duration-300 cursor-pointer">
+                            <Image
+                                src="/icons/icono-email.svg"
+                                alt="Icono Email"
+                                width={36}
+                                height={36}
+                            />
+                            <p className="text-[1.1rem] font-[600] m-[0px] pr-[1.6rem]">evagandiabelda@gmail.com</p>
                         </div>
 
                     </div>
